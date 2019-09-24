d6b518c2a28051925791ddf15019aaad1feaa947
Eva <ewelin2004@bk.ru>
2019-09-24 18:41:27 +0300

    Генерация магов с рандомными характеристиками 1.0

--- js/setup.js
+++ js/setup.js
@@ -1,5 +1,7 @@
 'use strict';
+
 (function () {
+  var QUANTITY = 4;
   var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
   var WIZARD_SURNAME = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
   var WIZARD_COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
@@ -12,25 +14,21 @@
     .content
     .querySelector('.setup-similar-item');
 
-  var fragment = document.createDocumentFragment();
-  var randomCharacteristic = [];
-  var randomWizardList = [];
-
   // функция, которая создает массив, состоящий из рандомных характеристик мага
   function getRandCharacteristics(wizardNames, wizardSurnames, wizardCoatColor, wizardEyesColor) {
     var randName = wizardNames[Math.floor(Math.random() * wizardNames.length)];
     var randSurname = wizardSurnames[Math.floor(Math.random() * wizardSurnames.length)];
     var randCoatColor = wizardCoatColor[Math.floor(Math.random() * wizardCoatColor.length)];
     var randEyesColor = wizardEyesColor[Math.floor(Math.random() * wizardEyesColor.length)];
-
     var randomCharacteristics = [randName, randSurname, randCoatColor, randEyesColor];
-
     return randomCharacteristics;
   }
 
   // фунцкия, которая создает рандомного мага и добавляет его в массив
   var getRandomWizard = function () {
-    for (var i = 0; i < 4; i++) {
+    var randomCharacteristic = [];
+    var randomWizardList = [];
+    for (var i = 0; i < QUANTITY; i++) {
       randomCharacteristic[i] = getRandCharacteristics(WIZARD_NAMES, WIZARD_SURNAME, WIZARD_COAT_COLOR, WIZARD_EYES_COLOR);
       var randomWizard = {
         name: randomCharacteristic[i][0] + ' ' + randomCharacteristic[i][1],
@@ -41,27 +39,24 @@
     }
     return randomWizardList;
   };
-  getRandomWizard();
 
   //  функция, которая создает DOM-элемент на основе JS-объекта
   var renderWizard = function (wizard) {
     var wizardElement = similarWizardTemplate.cloneNode(true);
-
     wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
     wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
     wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
-
     return wizardElement;
   };
 
   // функция отрисовки шаблона в документ
-  var drawingTemplToDocument = function () {
-    for (var i = 0; i < randomWizardList.length; i++) {
-      fragment.appendChild(renderWizard(randomWizardList[i]));
+  var drawingTemplToDocument = function (wizard) {
+    var fragment = document.createDocumentFragment();
+    for (var i = 0; i < wizard.length; i++) {
+      fragment.appendChild(renderWizard(wizard[i]));
     }
-    similarListElement.appendChild(fragment);
+    return fragment;
   };
-  drawingTemplToDocument();
-
   userDialog.querySelector('.setup-similar').classList.remove('hidden');
+  similarListElement.appendChild(drawingTemplToDocument(getRandomWizard()));
 })();
